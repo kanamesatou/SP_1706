2ab1d3058ea0c676fe31f353d215bb1d59cb3dea
satou kaname <satou2018hcs@gmail.com>
2017-10-22 00:30:03 +0900

    ChatController追加、routesに追記

---
+++ app/controllers/ChatController.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import models.data.User
+import play.api.mvc._
+
+/**
+  * Created by satou on 2017/10/21.
+  */
+class ChatController extends Controller {
+
+  def chatRoom(url: String) = Action { implicit request =>
+    User.auth(url) { user =>
+      println(user)
+      Ok("printed")
+    }
+  }
+
+}
