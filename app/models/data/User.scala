ba0d32e23d7221ee1313d3206cb4057c5674ffcd
satou kaname <satou2018hcs@gmail.com>
2017-10-22 01:19:14 +0900

    User#authenticateのリファクタリング

--- app/models/data/User.scala
+++ app/models/data/User.scala
@@ -6,6 +6,8 @@ import play.api.mvc.{Controller, Request, Result}
 import services.UserService
 import util.Extension.FormExtension
 
+import scala.util.Try
+
 /**
   * Created by satou on 2017/10/21.
   */
@@ -15,7 +17,11 @@ object User {
   private def sessionName(url: String): String = s"pseudcussion[$url]"
 
   def authenticate[A](url: String)(ifSuccess: User => A)(ifFailed: => A)(implicit request: Request[_]): A =
-    request.session.get(sessionName(url)).fold(ifFailed)(id => ifSuccess(UserService.findById(id.toLong)))
+    request.session
+      .get(sessionName(url))
+      .flatMap(id => Try(id.toLong).toOption)
+      .flatMap(UserService.findById)
+      .fold(ifFailed)(ifSuccess)
 
   def auth(url: String)(ifSuccess: User => Result)(implicit request: Request[_], controller: Controller): Result =
     authenticate(url)(ifSuccess)(controller.Ok("")) // TODO login page
