b5b47c0cf36cd1b492c5493d271297ec9a4475ff
satou kaname <satou2018hcs@gmail.com>
2017-10-22 00:28:35 +0900

    RoomServiceにfindByUrl追加

--- app/services/RoomService.scala
+++ app/services/RoomService.scala
@@ -8,16 +8,23 @@ import scala.collection.mutable.ListBuffer
   * Created by satou on 2017/10/21.
   */
 trait RoomRepository {
-  /**
-    * URLに対応するRoomを返す
-    */
-  def findByUrl(url: String): Option[Room]
 
   /**
     * Roomを登録する
     * URLの重複があった場合はエラーメッセージを返す
     */
   def entry(room: Room.Form): Option[String]
+
+  /**
+    * URLに対応するRoomを返す
+    */
+  def findByUrl(url: String): Option[Room]
+
+  /**
+    * idに対応するRoomを返す
+    */
+  def findById(id: Long): Option[Room]
+
 }
 
 trait UsesRoomRepository {
@@ -28,9 +35,13 @@ trait UsesRoomRepository {
   * DBを使ったRoomRepositoryの実装
   */
 object RoomRepositoryImpl extends RoomRepository {
-  def findByUrl(url: String): Option[Room] = ???
 
   def entry(room: Room.Form): Option[String] = ???
+
+  def findByUrl(url: String): Option[Room] = ???
+
+  def findById(id: Long): Option[Room] = ???
+
 }
 
 /**
@@ -39,8 +50,6 @@ object RoomRepositoryImpl extends RoomRepository {
 object RoomRepositoryMock extends RoomRepository {
   private val repo = ListBuffer.empty[Room]
 
-  def findByUrl(url: String): Option[Room] = repo.find(_.url == url)
-
   def entry(roomForm: Room.Form): Option[String] = repo.synchronized {
     findByUrl(roomForm.url).map(_ => s"このURLは既に登録されています").fold[Option[String]] {
       val room = Room(repo.length, roomForm.url, roomForm.name, roomForm.password)
@@ -48,6 +57,10 @@ object RoomRepositoryMock extends RoomRepository {
       None
     }(Some.apply)
   }
+
+  def findByUrl(url: String): Option[Room] = repo.find(_.url == url)
+
+  def findById(id: Long): Option[Room] = repo.find(_.id == id)
 }
 
 trait MixInRoomRepository {
@@ -67,6 +80,12 @@ trait RoomService extends UsesRoomRepository {
     * 存在する場合はRoom, 存在しない場合はNoneを返す
     */
   def findByUrl(url: String): Option[Room] = roomRepository.findByUrl(url)
+
+  /**
+    * idに対応するRoomを返す
+    * 存在する場合はRoom, 存在しない場合はNoneを返す
+    */
+  def findById(id: Long): Option[Room] = roomRepository.findById(id)
 
 }
 
