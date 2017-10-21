37890b4af065ea30f5f302a03abae3b6ae65613e
satou kaname <satou2018hcs@gmail.com>
2017-10-22 01:18:37 +0900

    RoomRepositoryMockの変更に伴う変更、RoomServiceSpecにid検索のテストを追加

--- test/services/RoomServiceSpec.scala
+++ test/services/RoomServiceSpec.scala
@@ -8,24 +8,31 @@ import org.scalatestplus.play.PlaySpec
   */
 class RoomServiceSpec extends PlaySpec {
 
-  object RoomService extends RoomService with MixInRoomRepository {
-    override val roomRepository: RoomRepository = RoomRepositoryMock
+  object RS extends RoomService with MixInRoomRepository {
+    override val roomRepository: RoomRepository = new RoomRepositoryMock
   }
 
   "RoomService" should {
-    val room1 = Room.Form("test/url", "テストルーム", "password")
+    val room1 = Room.Form("/test/url", "テストルーム", "password")
     val room2 = room1.copy(url = "/test/url/xxx")
 
     "Roomのエントリー" in {
-      RoomService.entry(room1) mustBe None
-      RoomService.entry(room2) mustBe None
-      RoomService.entry(room1).getClass mustBe classOf[Some[String]]
+      RS.entry(room1) mustBe None
+      RS.entry(room2) mustBe None
+      RS.entry(room1).getClass mustBe classOf[Some[String]]
     }
 
-    "Roomの検索" in {
-      RoomService.findByUrl("/test/url") mustBe Some(room1)
-      RoomService.findByUrl("/test/url/xxx") mustBe Some(room2)
-      RoomService.findByUrl("/test/url/xxx/yyy") mustBe None
+    "RoomのURL検索" in {
+      RS.findByUrl("/test/url") mustBe Some(Room(0, "/test/url", "テストルーム", "password"))
+      RS.findByUrl("/test/url/xxx") mustBe Some(Room(1, "/test/url/xxx", "テストルーム", "password"))
+      RS.findByUrl("/test/url/xxx/yyy") mustBe None
+    }
+
+    "Roomのid検索" in {
+      RS.findById(-1) mustBe None
+      RS.findById(0) mustBe Some(Room(0, "/test/url", "テストルーム", "password"))
+      RS.findById(1) mustBe Some(Room(1, "/test/url/xxx", "テストルーム", "password"))
+      RS.findById(2) mustBe None
     }
 
   }
