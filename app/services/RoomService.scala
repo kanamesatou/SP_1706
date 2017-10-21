91da779184556d93585d1b0d204a1a2033570ba0
satou kaname <satou2018hcs@gmail.com>
2017-10-21 21:46:42 +0900

    RoomServiceの追加

---
+++ app/services/RoomService.scala
@@ -0,0 +1,67 @@
+package services
+
+import models.data.Room
+import scala.collection.mutable.ListBuffer
+
+
+/**
+  * Created by satou on 2017/10/21.
+  */
+trait RoomRepository {
+  /**
+    * URLに対応するRoomを返す
+    */
+  def findByUrl(url: String): Option[Room]
+
+  /**
+    * Roomを登録する
+    * URLの重複があった場合はエラーメッセージを返す
+    */
+  def entry(room: Room): Option[String]
+}
+
+trait UsesRoomRepository {
+  val roomRepository: RoomRepository
+}
+
+/**
+  * DBを使ったRoomRepositoryの実装
+  */
+object RoomRepositoryImpl extends RoomRepository {
+  def findByUrl(url: String): Option[Room] = ???
+
+  def entry(room: Room): Option[String] = ???
+}
+
+/**
+  * DBを使わないRoomRepositoryの実装
+  */
+object RoomRepositoryMock extends RoomRepository {
+  private val repo = ListBuffer.empty[Room]
+
+  def findByUrl(url: String): Option[Room] = repo.find(_.url == url)
+
+  def entry(room: Room): Option[String] = findByUrl(room.url).map(_ => s"このURLは既に登録されています")
+}
+
+trait MixInRoomRepository {
+  val roomRepository: RoomRepository = RoomRepositoryMock
+}
+
+trait RoomService extends UsesRoomRepository {
+
+  /**
+    * Roomをリポジトリに登録する
+    * 登録成功時はNone, 失敗時はエラーメッセージを返す
+    */
+  def entry(room: Room): Option[String] = roomRepository.entry(room)
+
+  /**
+    * URLに対応するRoomを返す
+    * 存在する場合はRoom, 存在しない場合はNoneを返す
+    */
+  def findByUrl(url: String): Option[Room] = roomRepository.findByUrl(url)
+
+}
+
+object RoomService extends RoomService with MixInRoomRepository
