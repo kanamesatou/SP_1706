fa19b836721dc1e82595d754da9944b274472a37
satou kaname <satou2018hcs@gmail.com>
2017-10-22 16:39:29 +0900

    Chat.PostForm追加、Chat.AjaxForm追加

--- app/models/data/Chat.scala
+++ app/models/data/Chat.scala
@@ -1,7 +1,46 @@
 package models.data
 
+import play.api.data.Forms._
+import play.api.data
+import play.api.data.format.Formats._
+import util.Extension.FormExtension
+
 /**
   * Created by satou on 2017/10/21.
   */
 case class Chat(id: Long, roomId: Long, no: Int, userId: Long,
                 timeStamp: String, replyTo: Option[Long], content: String)
+
+object Chat {
+
+  case class PostForm(roomId: Long, userId: Long, replyTo: Option[Long], content: String)
+
+  object PostForm extends FormExtension[PostForm] {
+    val roomId = "roomId"
+    val userId = "userId"
+    val replyTo = "replyTo"
+    val content = "content"
+
+    val form = data.Form(
+      mapping(
+        roomId -> of[Long],
+        userId -> of[Long],
+        replyTo -> optional(of[Long]),
+        content -> text
+      )(PostForm.apply)(PostForm.unapply)
+    )
+  }
+
+  case class AjaxForm(roomId: Long)
+
+  object AjaxForm extends FormExtension[AjaxForm] {
+    val roomId = "roomId"
+
+    val form = data.Form(
+      mapping(
+        roomId -> of[Long]
+      )(AjaxForm.apply)(AjaxForm.unapply)
+    )
+  }
+
+}
