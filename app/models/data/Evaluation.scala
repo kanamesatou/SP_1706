cc4acd5a0411c431a37027a0a08ddb017e6c0281
satou kaname <satou2018hcs@gmail.com>
2017-10-25 16:05:33 +0900

    EvaluationにnoとFormオブジェクトを追加

--- app/models/data/Evaluation.scala
+++ app/models/data/Evaluation.scala
@@ -1,13 +1,38 @@
 package models.data
 
+import play.api.data
+import play.api.data.Forms._
+import play.api.data.format.Formats._
+import util.Extension.FormExtension
+
 /**
   * Created by satou on 2017/10/21.
   */
-case class Evaluation(id: Long, chatId: Long, userId: Long, evaluationType: Evaluation.Type)
+case class Evaluation(id: Long, roomId: Long, no: Int, userId: Long, evaluationType: Evaluation.Type)
 
 object Evaluation {
 
   sealed trait Type
+
+  case class Form(roomId: Long, no: Int, userId: Long, evaluationType: String)
+
+  object Form extends FormExtension[Form] {
+
+    val roomId = "roomId"
+    val no = "no"
+    val userId = "userId"
+    val evaluationType = "evaluationType"
+
+    val form = data.Form(
+      mapping(
+        roomId -> of[Long],
+        no -> number,
+        userId -> of[Long],
+        evaluationType -> text
+      )(Form.apply)(Form.unapply)
+    )
+  }
+
 
   object Type {
 
